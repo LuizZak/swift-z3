a5269b4d7b01516352156533f473e275ee344710
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2020-02-04 02:04:42 -0300

    Implementing Z3Params and Z3ParamDescrs

---
+++ Sources/SwiftZ3/Z3Params.swift
@@ -0,0 +1,49 @@
+import Z3
+
+public class Z3Params {
+    var context: Z3Context
+    var params: Z3_params
+    
+    init(context: Z3Context, params: Z3_params) {
+        self.context = context
+        self.params = params
+        Z3_params_inc_ref(context.context, params)
+    }
+    
+    deinit {
+        Z3_params_dec_ref(context.context, params)
+    }
+    
+    /// Add a Boolean parameter `k` with value `v` to the parameter set `p`.
+    public func setBool(_ symbol: Z3Symbol, _ value: Bool) {
+        Z3_params_set_bool(context.context, params, symbol.symbol, value)
+    }
+    
+    /// Add an unsigned parameter `k` with value `v` to the parameter set `p`.
+    public func setUInt(_ symbol: Z3Symbol, _ value: UInt32) {
+        Z3_params_set_uint(context.context, params, symbol.symbol, value)
+    }
+    
+    /// Add a double parameter `k` with value `v` to the parameter set `p`.
+    public func setDouble(_ symbol: Z3Symbol, _ value: Double) {
+        Z3_params_set_double(context.context, params, symbol.symbol, value)
+    }
+    
+    /// Add a symbol parameter `k` with value `v` to the parameter set `p`.
+    public func setSymbol(_ symbol: Z3Symbol, _ value: Z3Symbol) {
+        Z3_params_set_symbol(context.context, params, symbol.symbol, value.symbol)
+    }
+    
+    /// Convert a parameter set into a string. This function is mainly used for
+    /// printing the contents of a parameter set.
+    public func toString() -> String {
+        return String(cString: Z3_params_to_string(context.context, params))
+    }
+    
+    /// Validate this parameter set against the parameter description set `d`.
+    ///
+    /// The procedure invokes the error handler if `self` is invalid.
+    public func validate(_ d: Z3ParamDescrs) {
+        Z3_params_validate(context.context, params, d.descrs)
+    }
+}
