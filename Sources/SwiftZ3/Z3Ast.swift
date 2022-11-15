0ed1642797828033fc064e2a748164a766a6c4f3
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-11-15 18:31:47 -0300

    Adding assertion in Z3Ast<T> initializer to check for underlying sort compatibility

--- Sources/SwiftZ3/Z3Ast.swift
+++ Sources/SwiftZ3/Z3Ast.swift
@@ -1,6 +1,18 @@
 import CZ3
 
 public class Z3Ast<T: SortKind>: AnyZ3Ast {
+    override init(context: Z3Context, ast: Z3_ast) {
+        assert(
+            T.isAssignableFrom(
+                context,
+                context.getSort(Z3AstBase(context: context, ast: ast))
+            ),
+            "Attempted to initialize \(Self.self) AST with incompatible Z3 sort \(context.getSort(Z3AstBase(context: context, ast: ast)))"
+        )
+
+        super.init(context: context, ast: ast)
+    }
+
     /// Translate/Copy the AST `self` from its current context to context `target`
     public override func translate(to newContext: Z3Context) -> Z3Ast {
         if context === newContext {
