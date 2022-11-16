7d496673883d457405bf3eb04f8ec61fb5b4f6ea
Luiz Fernando Silva <luizinho_mack@yahoo.com.br>
2022-11-16 03:11:40 -0300

    Adding Swift API for Z3_global_param_* functions

--- Sources/SwiftZ3/Z3Config.swift
+++ Sources/SwiftZ3/Z3Config.swift
@@ -1,5 +1,29 @@
 import CZ3
 
+/// A configuration object for the Z3 context object.
+///
+/// Configurations are created in order to assign parameters prior to creating
+/// contexts for Z3 interaction. For example, if the users wishes to use proof
+/// generation, then call:
+///
+/// `Z3Config.setParameter("proof", "true")`
+///
+/// - remark: In previous versions of Z3, the `Z3_config` was used to store
+/// global and module configurations. Now, we should use `Z3GlobalParameters.setParameter(id:value:)`.
+///
+/// The following parameters can be set:
+///
+/// - `proof`:  (Boolean)           Enable proof generation
+/// - `debug_ref_count`: (Boolean)  Enable debug support for Z3_ast reference counting
+/// - `trace`:  (Boolean)           Tracing support for VCC
+/// - `trace_file_name`: (String)   Trace out file for VCC traces
+/// - `timeout`: (unsigned)         default timeout (in milliseconds) used for solvers
+/// - `well_sorted_check`:          type checker
+/// - `auto_config`:                use heuristics to automatically select solver and configure it
+/// - `model`:                      model generation for solvers, this parameter can be overwritten when creating a solver
+/// - `model_validate`:             validate models produced by solvers
+/// - `unsat_core`:                 unsat-core generation for solvers, this parameter can be overwritten when creating a solver
+/// - `encoding`:                   the string encoding used internally (must be either "unicode" - 18 bit, "bmp" - 16 bit or "ascii" - 8 bit)
 public class Z3Config {
     internal var config: Z3_config
 
---
+++ Sources/SwiftZ3/Z3GlobalParameters.swift
@@ -0,0 +1,56 @@
+import CZ3
+
+/// Contains static APIs for configuring global parameters for all Z3 contexts.
+public enum Z3GlobalParameters {
+    /// - Restore the value of all global (and module) parameters.
+    /// This command will not affect already created objects (such as tactics and solvers).
+    ///
+    /// - seealso: `getParameter`
+    /// - seealso: `setParameter`
+    public static func resetAll() {
+        Z3_global_param_reset_all()
+    }
+
+    /// Get a global (or module) parameter.
+    ///
+    /// Returns `nil` if the parameter value does not exist.
+    ///
+    /// - seealso: `resetAll`
+    /// - seealso: `setParameter`
+    ///
+    /// - remark: This function cannot be invoked simultaneously from different threads without synchronization.
+    /// The result string stored in param_value is stored in shared location.
+    public static func getParameter(id: String) -> String? {
+        var str: Z3_string?
+
+        if Z3_global_param_get(id, &str), let str = str {
+            return str.toString()
+        }
+
+        return nil
+    }
+    
+    /// Set a global (or module) parameter.
+    /// This setting is shared by all Z3 contexts.
+    ///
+    /// When a Z3 module is initialized it will use the value of these parameters
+    /// when Z3_params objects are not provided.
+    ///
+    /// The name of parameter can be composed of characters [a-z][A-Z], digits [0-9], '-' and '_'.
+    /// The character '.' is a delimiter (more later).
+    ///
+    /// The parameter names are case-insensitive. The character '-' should be viewed as an "alias" for '_'.
+    /// Thus, the following parameter names are considered equivalent: "pp.decimal-precision"  and "PP.DECIMAL_PRECISION".
+    ///
+    /// This function can be used to set parameters for a specific Z3 module.
+    /// This can be done by using <module-name>.<parameter-name>.
+    /// For example:
+    /// `setParameter(id: "pp.decimal", "true")`
+    /// will set the parameter "decimal" in the module "pp" to true.
+    ///
+    /// - seealso: `getParameter`
+    /// - seealso: `resetAll`
+    public static func setParameter(id: String, value: String) {
+        Z3_global_param_set(id, value)
+    }
+}
